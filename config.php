728c1efb3e9ce47c8ffd71aa076f9bd14fca43b5
Nam Sook Jing <148428419+namsookjing@users.noreply.github.com>
2025-05-21 14:56:44 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
+$port = getenv('DB_PORT');
+
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+if ($conn->connect_error) {
+    die("ERROR: " . $conn->connect_error);
+}
+echo "Successfully Connect";
+?>
