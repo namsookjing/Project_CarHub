72dab692ea392834c93d53b3f17c4e1be0e379a4
Nam Sook Jing <148428419+namsookjing@users.noreply.github.com>
2025-05-21 15:28:28 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include 'config.php';
+
+$sql = "SELECT * FROM your_table";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "Data: " . json_encode($row) . "<br>";
+    }
+} else {
+    echo "No results";
+}
+?>
